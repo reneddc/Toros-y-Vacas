941e239df35b1d59bb59976777a1574340ff91b3
“Rene <rene.diaz@ucb.edu.bo>
2022-04-10 22:03:10 -0400

    (HDU 2) Interfaz de Usuario

--- src/presenter.js
+++ src/presenter.js
@@ -2,14 +2,21 @@ import VacasToros from './VacasToros'
 
 const form = document.querySelector("#vista-1-form");
 const div = document.querySelector("#visualizaciones");
+
 const inputNumeroCaracteres = document.querySelector("#numero-caracteres");
+const inputNumeroIntentos = document.querySelector("#numero-intentos");
+
 let vacasToros = new VacasToros();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let numeroCaracteres = inputNumeroCaracteres.value;
-  vacasToros.definirNumeroDeCaracteres(numeroCaracteres);
+  let numeroIntentos = inputNumeroIntentos.value;
 
-  div.innerHTML = "<p>" + vacasToros.getNumeroDeCaracteres() + "</p>";
+  vacasToros.definirNumeroDeCaracteres(numeroCaracteres);
+  vacasToros.definirNumeroDeIntentos(numeroIntentos);
+
+  div.innerHTML =  `<p> ${vacasToros.getNumeroDeCaracteres()} </p>
+                    <p> ${vacasToros.getNumeroDeIntentos()} </p>`;
 });
