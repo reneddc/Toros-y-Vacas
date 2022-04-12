37883c087a663081a9027ea4e5fab1e1a83c96d7
Rene Diaz <ddcrene@gmail.com>
2022-04-12 12:02:20 -0400

    EJ 3 PRUEBA CORRECTA

--- src/VacasToros.test.js
+++ src/VacasToros.test.js
@@ -373,10 +373,11 @@ describe("VACAS Y TOROS", () => {
     });
 
     it("ej.1. Si el código secreto es <1 2 3 4>, el tipo de código es “numeros” y se ingresa como intento el código <2 5 7 8>, debería mostrar la respuesta como <# o o o>, sólo aplicable en la primera posición", () => {
-        vacasToros.definirConfiguracionTotal(4, 6, "Letras", false);
-        vacasToros.definirCodigoSecreto(['1', '2', '3', '4']);
+        vacasToros.definirConfiguracionTotal(4, 6, "Numeros", false);
+        vacasToros.definirCodigoSecreto(['1', '2', '3', '9']);
         vacasToros.definirIntento(['2', '3', '4', '0']);
         respuesta = vacasToros.getResultadoDeIntento();
-        expect(respuesta).toEqual(['O', 'V', '#', 'O']);
+        console.log(respuesta);
+        expect(respuesta).toEqual(['V', 'V', '#', 'O']);
     });
 });
--- src/funcionesJuegoVacasToros.js
+++ src/funcionesJuegoVacasToros.js
@@ -114,6 +114,7 @@ class FuncionesDeJuego{
     generarResultadoDeIntento(numeroCaracteres, resultadoIntento, codigoSecreto, intentoCodigo){
         if(!this.esError(resultadoIntento)){
             resultadoIntento = this.generarCodigoVacioYLleno(numeroCaracteres, "O");
+            resultadoIntento = this.generarTernera(codigoSecreto, intentoCodigo, resultadoIntento);
             resultadoIntento = this.generarToros(codigoSecreto, intentoCodigo, resultadoIntento);
             resultadoIntento = this.generarVacas(codigoSecreto, intentoCodigo, resultadoIntento);
             this.numeroIntentosRealizados++;
